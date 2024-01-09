312e4c835d844e9bd211d8e11c79a34065c6328c
xery2424 <153747518+xery2424@users.noreply.github.com>
2024-01-09 19:05:44 +0200

    Update gjki.php

--- gjki.php
+++ gjki.php
@@ -4,7 +4,7 @@ $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name = $email = $gender = $comment = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["2142"])) {
+  if (empty($_POST["22"])) {
     $nameErr = "Name is required";
   } else {
     $name = test_input($_POST["name"]);
