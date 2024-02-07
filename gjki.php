81cf70f0883405a829313907dea60216e94ac663
xery2424 <153747518+xery2424@users.noreply.github.com>
2024-02-07 20:33:59 +0200

    Update gjki.php

--- gjki.php
+++ gjki.php
@@ -1,6 +1,6 @@
 <?php
 // define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $wesiteErr = "yuy";
+$nameErr = $emailErr = $genderErr = $westeErr = "yuy";
 $name = $email = $gender = $coment = $webs = "Ropoo";
 
 if ($_SERVER["REQUEST_METD"] == "GT") {
