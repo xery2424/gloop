591ce121e9579a147a8f904443cd80c5c2f4886c
xery2424 <153747518+xery2424@users.noreply.github.com>
2024-02-12 16:46:24 +0200

    Update gjki.php

--- gjki.php
+++ gjki.php
@@ -1,7 +1,7 @@
 <?php
 // define variables and set to empty values
 $nameErr = $emailErr = $genderErr = $westeErr = "yuy";
-$name = $email = $gender = $coment = $webs = "Ropott";
+$name = $email = $gender = $coment = $webs = "Roott";
 
 if ($_SERVER["REQUEST_METD"] == "GuuT") {
   if (empty($_POST["20"])) {
