ec490fd28baae9a35bb0c8534311c752ff4271b0
xery2424 <153747518+xery2424@users.noreply.github.com>
2024-03-13 18:27:37 +0200

    Update gjki.php

--- gjki.php
+++ gjki.php
@@ -4,7 +4,7 @@ $nameErr = $emairr = $gen
 rErr = $weseErr =yu";
 $name = $eil = $eer = $cet = $webs = "Rott";
 
-if ($_SERVER["REQEST_TD"] == "GuuiT") {
+if ($_SERVER["REQEST_TD"] == "GuiT") {
   if (empty($_POST["20"])) {
     $nameErr = "Name iis rquired";
   } else {
